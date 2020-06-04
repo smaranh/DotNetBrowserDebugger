e7c3ac3e66d8a76900032655accc795c7a827975
Smaran Harihar <sharihar@esri.com>
2020-06-04 11:35:19 -0700

    winform to use as a debugger

---
+++ DotNetBrowserDebugger/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ESRI.ArcGIS.Mapping.OfficeIntegration.Core
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.browserView1 = new DotNetBrowser.WinForms.BrowserView();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // browserView1
+            // 
+            this.browserView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browserView1.Location = new System.Drawing.Point(0, 22);
+            this.browserView1.Name = "browserView1";
+            this.browserView1.Size = new System.Drawing.Size(800, 428);
+            this.browserView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.browserView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private DotNetBrowser.WinForms.BrowserView browserView1;
+    }
+}
+
---
+++ DotNetBrowserDebugger/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Forms;
+using DotNetBrowser.Engine;
+using DotNetBrowser.Browser;
+
+namespace ESRI.ArcGIS.Mapping.OfficeIntegration.Core
+{
+    public partial class Form1 : Form
+    {
+        IEngine Engine;
+        IBrowser browser;
+        public Form1()
+        {
+            Engine = createEngine(@"C:\DEV\testing\dotnetbrowser\DotNetBrowserDebugger\cache");
+            Task.Run(() =>
+                Engine.CreateBrowser()
+            ).ContinueWith(t =>
+            {
+                browser = t.Result;
+                browserView1.InitializeFrom(browser);
+                browser.Navigation.LoadUrl("http://localhost:4500");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+            InitializeComponent();
+        }
+
+        public static IEngine createEngine(string userdatadir)
+        {
+            IEngine engine = EngineFactory.Create(new EngineOptions.Builder
+            {
+                RenderingMode = RenderingMode.HardwareAccelerated,
+                ChromiumDirectory = @"C:\DEV\testing\dotnetbrowser\DotNetBrowserDebugger\binaries",
+                UserDataDirectory = userdatadir
+            }
+            .Build());
+            return engine;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Engine?.Dispose();
+            browser?.Dispose();
+        }
+    }
+}
